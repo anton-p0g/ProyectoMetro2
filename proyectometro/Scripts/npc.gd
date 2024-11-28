40588c9cc7d1b7c4e61b15e9406c043d2eeeb6af
Jose Ma <therappero3423@icloud.com>
2024-11-28 23:13:55 +0100

    integracion 3d

--- proyectometro/Scripts/npc.gd
+++ proyectometro/Scripts/npc.gd
@@ -4,4 +4,4 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _process(delta: float) -> void:
 	var animation_player = get_node("AnimationPlayer")
-	animation_player.play("walk")
+	animation_player.play("idle")
