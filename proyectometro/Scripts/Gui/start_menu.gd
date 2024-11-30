2250d94a875a4ea1ec38c92ba4d647096e0adbba
eduvicgar <eduvg0102@gmail.com>
2024-11-30 13:13:25 +0100

    cambio título ventana

--- proyectometro/Scripts/Gui/start_menu.gd
+++ proyectometro/Scripts/Gui/start_menu.gd
@@ -3,6 +3,7 @@ extends Control
 signal loaded
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	DisplayServer.window_set_title("Calculadora de ruta Subte Buenos Aires")
 	$Menu.visible = false
 
 
