4d75caaba4e899fbb832c9dde23d1a7dfc7c07d4
eduvicgar <eduvg0102@gmail.com>
2024-11-30 12:33:04 +0100

    Revert "boton opcional"
    This reverts commit cc398abeea14be1781d1706683783c34b4bf746a.

--- proyectometro/Scripts/Gui/nodo_menu.gd
+++ proyectometro/Scripts/Gui/nodo_menu.gd
@@ -13,13 +13,10 @@ func _on_button_pressed() -> void:
 		animationPlayer.play("show_menu")
 
 
+
+
 func _on_main_menu_pressed() -> void:
-	GlobalData.animaciones = false
 	Transition.transition_to("res://Scenes/Gui/StartMenuScreen.tscn")
 
 func _on_exit_pressed() -> void:
-	if GlobalData.animaciones:
-		Transition.transition_to("res://Scenes/estacion_salida.tscn")
-		await get_tree().create_timer(0.5).timeout
-	else:
-		get_tree().quit()
+	Transition.transition_to("res://Scenes/estacion_salida.tscn")
